dded58c6e370dfe79607cff104d42de129ef3448
Michael Olie <molie@rarecollectiblestv.com>
2023-05-03 16:29:05 -0700

    added admin page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,14 @@
 import { FunctionComponent } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import AdminPage from "./pages/Admin";
 
 const AgendaRoutes: FunctionComponent = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/admin" element={<AdminPage />} />
     </Routes>
   </BrowserRouter>
 );
