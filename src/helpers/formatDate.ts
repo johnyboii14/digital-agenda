ff487c6676f761e3599eed6895d1ecbc8c422f65
Michael Olie <molie@rarecollectiblestv.com>
2023-08-04 11:12:41 -0700

    added time

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -20,10 +20,11 @@ function formatDateInPST(inputDate: string): string {
   const month = months[dateObj.getUTCMonth()];
   const year = dateObj.getUTCFullYear();
   const hour = dateObj.getUTCHours();
+  const minutes = dateObj.getUTCMinutes();
   const meridiem = hour >= 12 ? 'PM' : 'AM';
   const hour12 = hour % 12;
 
-  return `${month} ${day}, ${year} at ${hour12}${meridiem} PST`;
+  return `${month} ${day}, ${year} at ${hour12}:${minutes} ${meridiem} PST`;
 }
 
 export default formatDateInPST;
